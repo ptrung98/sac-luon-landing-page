a88794a37a57282e143c58b4a8cf7687dc575490
ptrung <phantrung8298@gmail.com>
2025-11-12 20:35:05 +0700

    update

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -13,7 +13,9 @@ export default function AboutPage() {
 
       <section className="content">
         <div className="about-us">
-          <div className="about-us-left"></div>
+          <div className="about-us-left">
+            <div className="about-us-left-image"></div>
+          </div>
           <div className="about-us-right">
             <h3 className="text-hightlight-half">VỀ CHÚNG TÔI</h3>
             <ul className="text-list-bullet-point">
@@ -29,40 +31,46 @@ export default function AboutPage() {
             </ul>
           </div>
         </div>
-        <div className="vision">
-          <h3>TẦM NHÌN</h3>
-          <ul className="text-list-bullet-point">
-            <li>
-              Sạc Luôn là thương hiệu cho thuê sạc dự phòng thông minh – giúp
-              bạn luôn đầy pin mọi lúc, mọi nơi.
-            </li>
-            <li>
-              Ra đời từ nhu cầu đơn giản nhưng thiết yếu trong cuộc sống hiện
-              đại: “Hết pin thì sao?”.
-            </li>
-            <li>
-              Chúng tôi mang đến giải pháp tiện lợi, nhanh chóng và thân thiện,
-              giúp bạn kết nối liên tục mà không bị gián đoạn – dù đang học tập,
-              làm việc hay di chuyển.
-            </li>
-          </ul>
-        </div>
-        <div className="mission">
-          <h3>SỨ MỆNH</h3>
-          <ul className="text-list-bullet-point">
-            <li>
-              Giữ năng lượng luôn sẵn sàng cho mọi người, mọi khoảnh khắc.
-            </li>
-            <li>
-              Chúng tôi tin rằng công nghệ nên phục vụ cuộc sống một cách gần
-              gũi và linh hoạt.
-            </li>
-            <li>
-              <strong>Sạc Luôn</strong> không chỉ cho thuê pin – mà mang đến
-              trải nghiệm năng lượng thông minh, giúp con người kết nối, chia sẻ
-              và sống năng động hơn mỗi ngày.
-            </li>
-          </ul>
+        <div className="vision-mission-container">
+          <div className="vision">
+            <h3>TẦM NHÌN</h3>
+            <ul className="text-list-bullet-point">
+              <li>
+                Sạc Luôn là thương hiệu cho thuê sạc dự phòng thông minh – giúp
+                bạn luôn đầy pin mọi lúc, mọi nơi.
+              </li>
+              <li>
+                Ra đời từ nhu cầu đơn giản nhưng thiết yếu trong cuộc sống hiện
+                đại: “Hết pin thì sao?”.
+              </li>
+              <li>
+                Chúng tôi mang đến giải pháp tiện lợi, nhanh chóng và thân
+                thiện, giúp bạn kết nối liên tục mà không bị gián đoạn – dù đang
+                học tập, làm việc hay di chuyển.
+              </li>
+            </ul>
+
+            <div className="image"></div>
+          </div>
+          <div className="mission">
+            <h3>SỨ MỆNH</h3>
+            <ul className="text-list-bullet-point">
+              <li>
+                Giữ năng lượng luôn sẵn sàng cho mọi người, mọi khoảnh khắc.
+              </li>
+              <li>
+                Chúng tôi tin rằng công nghệ nên phục vụ cuộc sống một cách gần
+                gũi và linh hoạt.
+              </li>
+              <li>
+                <strong>Sạc Luôn</strong> không chỉ cho thuê pin – mà mang đến
+                trải nghiệm năng lượng thông minh, giúp con người kết nối, chia
+                sẻ và sống năng động hơn mỗi ngày.
+              </li>
+            </ul>
+
+            <div className="image"></div>
+          </div>
         </div>
       </section>
     </main>
