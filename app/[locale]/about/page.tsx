81fcb110039231366f0e302eecf16c57c65e1df5
ptrung <phantrung8298@gmail.com>
2025-11-12 19:09:04 +0700

    add about page

---
+++ app/[locale]/about/page.tsx
@@ -0,0 +1,70 @@
+export default function AboutPage() {
+  return (
+    <main className="about">
+      <section className="hero">
+        <div className="overlay">
+          <span>
+            VỚI <span className="text-hightlight">SẠC LUÔN</span> <br />{" "}
+            <span className="text-hightlight">Năng lượng</span> không chỉ là pin{" "}
+            <br /> mà <span className="text-hightlight">là để Kết nối.</span>
+          </span>
+        </div>
+      </section>
+
+      <section className="content">
+        <div className="about-us">
+          <div className="about-us-left"></div>
+          <div className="about-us-right">
+            <h3 className="text-hightlight-half">VỀ CHÚNG TÔI</h3>
+            <ul className="text-list-bullet-point">
+              <li>
+                Chúng tôi tin rằng công nghệ nên phục vụ cuộc sống một cách gần
+                gũi và linh hoạt.
+              </li>
+              <li>
+                Sạc Luôn không chỉ cho thuê pin – mà mang đến trải nghiệm năng
+                lượng thông minh, giúp con người kết nối, chia sẻ và sống năng
+                động hơn mỗi ngày.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="vision">
+          <h3>TẦM NHÌN</h3>
+          <ul className="text-list-bullet-point">
+            <li>
+              Sạc Luôn là thương hiệu cho thuê sạc dự phòng thông minh – giúp
+              bạn luôn đầy pin mọi lúc, mọi nơi.
+            </li>
+            <li>
+              Ra đời từ nhu cầu đơn giản nhưng thiết yếu trong cuộc sống hiện
+              đại: “Hết pin thì sao?”.
+            </li>
+            <li>
+              Chúng tôi mang đến giải pháp tiện lợi, nhanh chóng và thân thiện,
+              giúp bạn kết nối liên tục mà không bị gián đoạn – dù đang học tập,
+              làm việc hay di chuyển.
+            </li>
+          </ul>
+        </div>
+        <div className="mission">
+          <h3>SỨ MỆNH</h3>
+          <ul className="text-list-bullet-point">
+            <li>
+              Giữ năng lượng luôn sẵn sàng cho mọi người, mọi khoảnh khắc.
+            </li>
+            <li>
+              Chúng tôi tin rằng công nghệ nên phục vụ cuộc sống một cách gần
+              gũi và linh hoạt.
+            </li>
+            <li>
+              <strong>Sạc Luôn</strong> không chỉ cho thuê pin – mà mang đến
+              trải nghiệm năng lượng thông minh, giúp con người kết nối, chia sẻ
+              và sống năng động hơn mỗi ngày.
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+  );
+}
